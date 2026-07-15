1e80e3fbd163319689883c83e9194abd16f3bec8
Phạm Văn Diệp <diepdz29012k7@gmail.com>
2026-07-15 12:50:59 +0700

    Delete schemas directory

--- schemas/course.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class CourseBase(BaseModel):
-    id: int
-    name: str
-    status: str
--- schemas/department.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-class DepartmentBase(BaseModel):
-    id: int
-    name: str
